70388aec53fb3282c5df7958927c7de31a385e24
whatevertogo <1879483647@qq.com>
2025-04-18 10:49:15 +0800

    1234

---
+++ Assets/Scripts/Camera/CameraControler.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControler : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+
+
+
+    public void Start()
+    {
+        Vector3 middleGrid = GridManager.Instance.GetGridCenter();
+        mainCamera.gameObject.transform.position = middleGrid + Vector3.forward * -10f;
+    }
+
+
+
+}
