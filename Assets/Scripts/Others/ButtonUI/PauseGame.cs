88d23e7b2c4ad799728107da4a4155bccb40e61b
whatevertogo <1879483647@qq.com>
2025-04-24 16:31:52 +0800

    feat: 更新暂停游戏功能，添加文本提示以指示游戏状态

--- Assets/Scripts/Others/ButtonUI/PauseGame.cs
+++ Assets/Scripts/Others/ButtonUI/PauseGame.cs
@@ -1,9 +1,11 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PauseGame : MonoBehaviour
 {
     private Button button;
+    [SerializeField] private TextMeshProUGUI textMeshProUGUI;
 
     private void Awake()
     {
@@ -15,10 +17,12 @@ public class PauseGame : MonoBehaviour
     {
         if (Time.timeScale == 1f)
         {
+            textMeshProUGUI.text = "继续游戏"; // 更新文本为“继续游戏”
             Time.timeScale = 0f; // 暂停游戏
         }
         else
         {
+            textMeshProUGUI.text = "暂停游戏"; // 更新文本为“暂停游戏”
             Time.timeScale = 1f; // 恢复游戏
         }
     }
