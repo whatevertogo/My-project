8c7bba958342c5d791db78bdbfdcc9e2d80698c3
whatevertogo <1879483647@qq.com>
2025-04-24 19:36:05 +0800

    refactor(CardContainer): 移除未使用的卡牌事件处理逻辑

--- Assets/Scripts/Card/CardContainer.cs
+++ Assets/Scripts/Card/CardContainer.cs
@@ -3,22 +3,4 @@ using UnityEngine;
 
 public class CardContainer : MonoBehaviour
 {
-    [SerializeField] private Card card;
-
-
-    private void Start()
-    {
-        card.OnCardPointing += CardPointingHandler;
-        card.OnCardNotPointing += CardNotPointingHandler;
-    }
-
-    private void CardNotPointingHandler(object sender, EventArgs e)
-    {
-        transform.SetAsFirstSibling(); //  放最下面s
-    }
-
-    private void CardPointingHandler(object sender, EventArgs e)
-    {
-        transform.SetAsLastSibling(); //  放最上面
-    }
 }
