06e25d6481b71941e140e247037037ea827787d3
whatevertogo <1879483647@qq.com>
2025-05-01 21:04:34 +0800

    feat: 更新等待播放时间，优化动画状态切换逻辑

--- Assets/Animations/Player/PlayerAnimationController.cs
+++ Assets/Animations/Player/PlayerAnimationController.cs
@@ -4,7 +4,7 @@ public class PlayerAnimationController : MonoBehaviour
 {
     private PlayerAnimationControl playerAnimationControl;
     [ReadOnly]
-    public float WaitToPlayTime = 1f; // 等待播放时间
+    public float WaitToPlayTime = 10f; // 等待播放时间
     private PlayerAnimationState lastState; // 记录上一次播放的动画
 
     private void Awake()
@@ -13,7 +13,10 @@ public class PlayerAnimationController : MonoBehaviour
         if (playerAnimationControl is null)
         {
             Debug.LogError("PlayerAnimationControl 未初始化！");
+            return;
         }
+        playerAnimationControl.SetPlayerAnimation(0, PlayerAnimationState.Standby1, true);
+
     }
 
     private void Update()
@@ -23,9 +26,10 @@ public class PlayerAnimationController : MonoBehaviour
             WaitToPlayTime -= Time.deltaTime;
             if (WaitToPlayTime <= 0)
             {
-                playerAnimationControl.SetPlayerAnimation(0, PlayerAnimationState.Standby1, true);
+                playerAnimationControl.SetPlayerAnimation(0, PlayerAnimationState.Standby2, false);
                 // 重置等待时间
-                WaitToPlayTime = Random.Range(1f, 3f);
+                WaitToPlayTime = Random.Range(7f, 10f);
+                playerAnimationControl.SetPlayerAnimation(0, PlayerAnimationState.Standby1, true);
             }
         }
     }
