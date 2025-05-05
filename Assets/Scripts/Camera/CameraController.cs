6fc7fd6d6abed6e77d75448dedb098e5df92bccb
whatevertogo <1879483647@qq.com>
2025-05-05 20:36:11 +0800

    fix: 调整预制件的旋转和位置以优化场景表现

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -7,12 +7,12 @@ public class CameraController : MonoBehaviour
     public void Start()
     {
         Vector3 middleGrid = GridManager.Instance.GetGridCenter();
-        mainCamera.gameObject.transform.position = middleGrid + Vector3.forward * -10f;
+        mainCamera.gameObject.transform.position = middleGrid + Vector3.forward * -7f;
     }
 
     private void Update()
     {
-        transform.position = new Vector3(Player.Instance.transform.position.x, Player.Instance.transform.position.y, -10f);
+        transform.position = new Vector3(Player.Instance.transform.position.x, Player.Instance.transform.position.y-7f, -10f);
     }
 
 
